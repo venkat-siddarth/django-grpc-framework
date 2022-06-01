2314abf3348ce6a68c6b1949c0230c2957d8d2f3
V Venkat Siddarth Reddy <60511416+venkat-siddarth@users.noreply.github.com>
2022-06-01 13:51:37 +0530

    Making it compatible for Prometheus

--- django_grpc_framework/management/commands/grpcrunserver.py
+++ django_grpc_framework/management/commands/grpcrunserver.py
@@ -73,8 +73,8 @@ class Command(BaseCommand):
         grpc_settings.ROOT_HANDLERS_HOOK(server)
         server.add_insecure_port(self.address)
         server.start(self.port)
-        start_http_server(53355)
-        print("The metrics server has started at localhost:53355")
+        start_http_server(self.port)
+        print(f"The metrics server has started at localhost:{self.port}")
         server.wait_for_termination()
 
     def inner_run(self, *args, **options):
